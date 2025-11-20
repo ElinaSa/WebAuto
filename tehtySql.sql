896d6db857befcfa5dbd5fa9469fd541ae7434bb
katalav <194138161+katalav@users.noreply.github.com>
2025-11-20 02:43:19 +0200

    tehty sql näkymä

--- tehtySql.sql
+++ tehtySql.sql
@@ -73,3 +73,26 @@ TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.jest_test
     OWNER to postgres;
+
+
+-- View: public.webajot
+
+-- DROP VIEW public.webajot;
+
+CREATE OR REPLACE VIEW public.webajot
+ AS
+ SELECT rekisterinumero,
+    tarkoitus,
+    (sukunimi::text || ' '::text) || etunimi::text AS nimi,
+    otto,
+    palautus
+   FROM ajopaivakirja
+  ORDER BY rekisterinumero, otto DESC;
+
+ALTER TABLE public.webajot
+    OWNER TO postgres;
+COMMENT ON VIEW public.webajot
+    IS 'tiivistetty ajopäiväkirja web-sovellusta varten';
+
+GRANT ALL ON TABLE public.webajot TO postgres;
+GRANT SELECT ON TABLE public.webajot TO websovellus;
