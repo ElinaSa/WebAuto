20ce2e52aa93071959967fc4df0277893f265eb9
Elina Saranki <156064412+ElinaSa@users.noreply.github.com>
2026-09-24 16:31:09 +0300

    korjaus

--- app.js
+++ app.js
@@ -23,10 +23,6 @@ const pgtools = require('./postgres-tools');
 // TODO tarkista 
 const { route } = require('express/lib/application'); 
 
-// TODO Onko oikeassa kohtaa ja oikeassa muodossa
-// Entä Headerit?
-const apiKey = await getSetting('API_KEY');
-
 
 // INITIALIZATION
 // --------------
